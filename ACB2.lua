799c290b46539cc4a5ddeab54d205cc5f4f88924
Pstrw <124980843+Pstrw@users.noreply.github.com>
2023-08-08 08:13:04 -0400

    Update ACB2.lua

--- ACB2.lua
+++ ACB2.lua
@@ -1,7 +1,9 @@
+getgenv().CoolCheck = false
+local groupId = 7548958
+
 local folder = Instance.new("Folder")
 folder.Name = "ABC"
 folder.Parent = game.Workspace
-
 local part = Instance.new("Part")
 part.Name = "ABCPart"
 part.Parent = folder
@@ -11,3 +13,15 @@ part.CastShadow = false
 part.Transparency = 1
 part.Size = Vector3.new(2048, 50, 2048)
 part.CFrame = CFrame.new(Vector3.new(-0.5, 38, -0.5))
+
+
+getgenv().CoolCheck = true
+
+while getgenv().CoolCheck == true do
+    wait()
+    for _, player in pairs(game.Players:GetPlayers()) do
+        if player:IsInGroup(groupId) then
+            game.Players.LocalPlayer:Kick(player.Name .. " Joined or Is In Your Game")
+        end
+    end
+end
